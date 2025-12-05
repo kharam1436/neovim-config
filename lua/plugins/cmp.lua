4ca09a3e9a36edfb7daa0fdc9f05a98bddc27f55
Haram Kwon <kharam1436@icloud.com>
2025-12-04 22:06:21 -0800

    chore: java formatter add

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,41 +1,41 @@
 -- lua/plugins/cmp.lua
 return {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-nvim-lsp", -- [중요] LSP 연동을 위해 이 플러그인이 꼭 필요합니다.
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "rafamadriz/friendly-snippets",
-    },
-    config = function()
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lsp", -- [중요] LSP 연동을 위해 이 플러그인이 꼭 필요합니다.
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 
-        require("luasnip.loaders.from_vscode").lazy_load()
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-k>"] = cmp.mapping.select_prev_item(),
-                ["<C-j>"] = cmp.mapping.select_next_item(),
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(), -- [수정] 키 바인딩 추가
-                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- [수정] 키 바인딩 추가
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-                { name = "buffer" },
-                { name = "path" },
-            }),
-        })
-    end,
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping.select_prev_item(),
+				["<C-j>"] = cmp.mapping.select_next_item(),
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(), -- [수정] 키 바인딩 추가
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- [수정] 키 바인딩 추가
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+		})
+	end,
 }
